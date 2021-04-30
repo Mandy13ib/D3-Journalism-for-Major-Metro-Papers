a3fb1cb844787af9945873547b33c6e3924677af
Mandy Farell <Mandy13ib@gmail.com>
2021-04-30 12:57:46 -0400

    adding code

--- D3_data/assets/js/app.js
+++ D3_data/assets/js/app.js
@@ -11,8 +11,7 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3
   .select("#scatter")
   .append("svg")
@@ -250,7 +249,8 @@ var ObeseLabel = ylabelsGroup.append("text")
   .attr("y", -80)
   .attr("x", -(height/2))
   .attr("dy", "1em")
-  .attr("value", "obesity") // value to grab for event listener
+  // value to grab for event listener
+  .attr("value", "obesity") 
   .classed("inactive", true)
   .text("Obese (%)");
 
@@ -258,7 +258,8 @@ var SmokesLabel = ylabelsGroup.append("text")
   .attr("y", -60)
   .attr("x", -(height/2))
   .attr("dy", "1em")
-  .attr("value", "smokes") // value to grab for event listener
+  // value to grab for event listener
+  .attr("value", "smokes") 
   .classed("inactive", true)
   .text("Smokes (%)");
 
@@ -266,7 +267,8 @@ var HealthLabel = ylabelsGroup.append("text")
   .attr("y", -40)
   .attr("x", -(height/2))
   .attr("dy", "1em")
-  .attr("value", "healthcare") // value to grab for event listener
+  // value to grab for event listener
+  .attr("value", "healthcare") 
   .classed("active", true)
   .text("Lacks Healthcare (%)");
 
@@ -278,23 +280,19 @@ xlabelsGroup.selectAll("text")
   .on("click", function() {
   // get value of selection
   var value = d3.select(this).attr("value");
+  
   if (value !== chosenXAxis) {
 
     // replaces chosenXAxis with value
     chosenXAxis = value;
-
     // functions here found above csv import and updates x scale for new data
     xLinearScale = xScale(data, chosenXAxis);
-
     // updates x axis with transition
     xAxis = renderXAxes(xLinearScale, xAxis);
-
     // updates circles with new x values
     circles = renderXCircles(circles, xLinearScale, chosenXAxis);
-
     // updating text within circles
     circlesText = renderXText(circlesText, xLinearScale, chosenXAxis)  
-
     // updates tooltips with new info
     circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
@@ -345,4 +343,57 @@ xlabelsGroup.selectAll("text")
 
       // replaces chosenYAxis with value
       chosenYAxis = value;
+      // functions here found above csv import and updates y scale for new data
+      yLinearScale = yScale(data, chosenYAxis);
+      // updates x axis with transition
+      yAxis = renderYAxes(yLinearScale, yAxis);
+      // updates circles with new y values
+      circles = renderYCircles(circles, yLinearScale, chosenYAxis);
+      // update text within circles
+      circlesText = renderYText(circlesText, yLinearScale, chosenYAxis) 
+      // updates tooltips with new info
+      circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
+      // changes classes to change bold text
+      if (chosenYAxis === "obesity") {
+        ObeseLabel
+          .classed("active", true)
+          .classed("inactive", false);
+        SmokesLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        HealthLabel
+          .classed("active", false)
+          .classed("inactive", true);
+      }
+
+      else if(chosenYAxis === 'smokes'){
+        SmokesLabel
+          .classed("active", true)
+          .classed("inactive", false);
+        HealthLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        ObeseLabel
+          .classed("active", false)
+          .classed("inactive", true);
+      }
+
+      else {
+        HealthLabel
+          .classed("active", true)
+          .classed("inactive", false);
+        SmokesLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        ObeseLabel
+          .classed("active", false)
+          .classed("inactive", true);
+      }
+    }
+  });
+
+
+}).catch(function(error) {
+  console.log(error);
+});
