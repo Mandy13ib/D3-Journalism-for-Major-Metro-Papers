02d59e2756fea715aa90772145848426cab7a98d
Mandy Farell <Mandy13ib@gmail.com>
2021-05-08 11:32:12 -0400

    working on flask

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,13 +1,10 @@
 //Set up chart
 var svgWidth = 960;
 var svgHeight = 500;
+
 //Setting up margins
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 50
-};
+var margin = { top: 20, right: 40, bottom: 80, left: 100 };
+
 //Setting up the width and height
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
@@ -17,12 +14,14 @@ var svg = d3
   .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  .attr("height", svgHeight)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+//append an SVG group
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-//Import the datat. Set the data to numerical values 
+//Import the data. Set the data to numerical values 
 d3.csv("assets/data/data.csv").then(function(CensusData) {
   CensusData.forEach(function(data) {
     data.age = +data.age;
@@ -52,31 +51,33 @@ d3.csv("assets/data/data.csv").then(function(CensusData) {
   chartGroup.append("g").call(yAxis);
 
 //set up what you are plotting on the  scatter plot
-chartGroup.selectAll("circle")
+chartGroup
+.selectAll("circle")
 .data(CensusData)
 .enter()
 .append("circle")
 .attr("cx", d=>xScale(d.age))
 .attr("cy", d=>yScale(d.smokes))
-.attr("r", "10")
+.attr("r", "12")
+.attr("fill", "lightblue")
 .attr("stroke-width", "1")
 .classed("stateCircle", true)
 .attr("opacity", 0.75);
 
 //add texts to each datapoint 
-chartGroup.append("g")
+chartGroup
+  .append("g")
   .selectAll('text')
   .data(CensusData)
   .enter()
   .append("text")
   .text(d=>d.abbr)
   .attr("x",d=>xScale(d.age))
+  .attr("fill", "white")
   .attr("y",d=>yScale(d.smokes))
   .classed(".stateText", true)
-  .attr("font-family", "sans-serif")
+  .style("font", "10px sans-serif")
   .attr("text-anchor", "middle")
-  .attr("fill", "white")
-  .attr("font-size", "10px")
   .style("font-weight", "bold")
   .attr("alignment-baseline", "central");
   
@@ -84,18 +85,18 @@ chartGroup.append("g")
   chartGroup.append("text")
         .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
         .attr("text-anchor", "middle")
-        .attr("font-size", "16px")
-        .attr("fill", "black")
+        .attr("fill", "dark blue")
         .style("font-weight", "bold")
+        .attr("font-size", "14px")
         .text("Median Age");
 
         chartGroup.append("text")
         .attr("y", 0 - ((margin.left / 2) + 2))
         .attr("x", 0 - (height / 2))
         .attr("text-anchor", "middle")
-        .attr("font-size", "16px")
-        .attr("fill", "black")
+        .attr("fill", "dark blue")
         .style("font-weight", "bold")
+        .attr("font-size", "14px")
         .attr("transform", "rotate(-90)")
         .text("Smokers (%)");
 }).catch(function(error) {
